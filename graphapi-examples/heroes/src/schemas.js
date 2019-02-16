90e701d7fbe4bb87ec7f575f6ac4c824b517a1c7
Erick Wendel <erick.workspace@gmail.com>
2019-02-16 12:36:25 -0800

    refactoring
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- graphapi-examples/heroes/src/schemas.js
+++ graphapi-examples/heroes/src/schemas.js
@@ -3,17 +3,13 @@ module.exports = gql`
 scalar Date
 
 type Query {
-    # create a new hero
     getHeroes(_id: String, name: String): [Hero!]
 }
 type Mutation {
-    # create a new hero
     createHero(power: [String!], birthday: Date, name: String): ID!
     
-    # delete hero by ID
     deleteHero(_id: ID!): Boolean!
 
-    ## test
     updateHero(_id: ID!, power: [String], birthday: Date, name: String): Boolean!
 
 }
